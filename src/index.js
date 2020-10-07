7fd3b1a0735d2c5b8eed54e9811e63698c00efb0
AlexUlan <alexander1.xata@gmail.com>
2020-10-07 23:51:24 +0300

    init : task reverse-init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let stringNumber = Math.abs(n).toString();
+    return stringNumber.split("").reverse().join("");
+};
